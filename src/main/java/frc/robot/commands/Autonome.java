15d69536877e402c431fd78402d4a2f73d013698
Falos732 <alexyouellet1@gmail.com>
2022-02-09 17:02:26 -0500

    autonome 1

--- src/main/java/frc/robot/commands/Autonome.java
+++ src/main/java/frc/robot/commands/Autonome.java
@@ -30,7 +30,7 @@ public class Autonome extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    m_drivetrain.drive(0.5, 0);
   }
 
   // Called once the command ends or is interrupted.

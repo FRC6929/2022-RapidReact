47d9cd8307c468e85aa87db8d22edb8c21ff52d7
Absolute99 <adamelfadil98@gmail.com>
2022-02-09 18:00:49 -0500

    final gyro pour la

--- src/main/java/frc/robot/subsystems/Gyro.java
+++ src/main/java/frc/robot/subsystems/Gyro.java
@@ -4,14 +4,23 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SPI;
+
+import com.kauailabs.navx.frc.AHRS;
 
 public class Gyro extends SubsystemBase {
+  private AHRS m_ahrs = new AHRS(SPI.Port.kMXP);
+
+  private static final int kGyroPort = 0;
+
   /** Creates a new Gyro. */
   public Gyro() {}
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("gyro angle", m_ahrs.getAngle());
     // This method will be called once per scheduler run
   }
 }

bf4d0599749d072dcc5bfc7f63c9c4a467268a0e
Falos732 <alexyouellet1@gmail.com>
2022-02-09 12:58:01 -0500

    autonome début

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.Autonome;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Drivetrain;
@@ -21,11 +22,12 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  public final Drivetrain m_drivetrain = new Drivetrain();
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();;
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Autonome m_autoCommand = new Autonome(m_drivetrain);
 
-  public final Drivetrain m_drivetrain = new Drivetrain();
 
   private final Joystick m_Joystick = new Joystick(0);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */

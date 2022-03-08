04b7396e8a831d6c0df5f39b53a7b42a699f1852
Falos732 <maxencek7@gmail.com>
2022-03-08 12:59:48 -0500

    limit switch test

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,22 +7,33 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
   private final CANSparkMax m_shooter1 = new CANSparkMax(Constants.ConsShooter.Moteur1, MotorType.kBrushless);
   private final CANSparkMax m_shooter2 = new CANSparkMax(Constants.ConsShooter.Moteur2, MotorType.kBrushless);
+  private final DigitalInput limit = new DigitalInput(1);
+
   /** Creates a new Shooter. */
 
   public Shooter() {}
 
+  public boolean getSwitch(){
+    return limit.get();
+  }
+
   @Override
   public void periodic() {
+    SmartDashboard.putBoolean("switch", getSwitch());
+
     // This method will be called once per scheduler run
   }
   public void shooterdrive(double speed) {
+    
     m_shooter1.set(speed);
     m_shooter2.set(-speed);
 

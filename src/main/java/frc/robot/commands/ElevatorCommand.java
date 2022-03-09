3b5192d39dc66f4f826ac9486a17425ba5ca2dcf
Falos732 <maxencek7@gmail.com>
2022-03-09 15:24:03 -0500

    enlever warning

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -4,10 +4,8 @@
 
 package frc.robot.commands;
 
-import frc.robot.Constants.ConsElevator;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
